1087c1bb9e59bcf945051112e0950792a181e5a4
u83697 <u83697@DS.UPF.EDU>
2016-03-10 12:15:20 +0100

    Custom map for vertices

--- Surface-Simplification/src/application.cpp
+++ Surface-Simplification/src/application.cpp
@@ -10,6 +10,22 @@ Mesh* mesh = NULL;
 Matrix44 model_matrix;
 Shader* phong = NULL;
 
+struct customVec3Comparator {
+    bool operator()(const Vector3& a, const Vector3& b) const {
+        return ((a.x < b.x) || (a.y < b.y) || (a.z < b.z));
+    }
+};
+
+void drawString(int x, int y, const char* string)
+{
+	int i, len;
+	glColor3f(0,0,0);
+	glRasterPos2f(x, y);
+	len = strlen (string);
+	for (i=0;i<len;i++)
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_10, string[i]);
+}
+
 Application::Application(const char* caption, int width, int height)
 {
 	this->window = createWindow(caption, width, height);
@@ -23,6 +39,17 @@ Application::Application(const char* caption, int width, int height)
 	this->window_width = w;
 	this->window_height = h;
 	this->keystate = SDL_GetKeyboardState(NULL);
+
+	Vector3 v1(0,0,0);
+	Vector3 v2(0,0,0);
+	Vector3 v3(1,2,3);
+
+	std::map<Vector3, int, customVec3Comparator> map;
+	map[v1] = 1;
+	map[v2] = 2;
+	map[v3] = 4;
+
+	std::cout << "Map size: " << map.size() << std::endl;
 }
 
 //Here we have already GL working, so we can create meshes and textures
@@ -73,6 +100,8 @@ void Application::render(void)
 	phong->setVector3("ambientColor", ambientColor);
 	phong->setVector3("diffuseColor", diffuseColor);
 	//render the data
+	drawString(0, 0, "Number of triangles: 0");
+	//drawString(10, 20, "Number of vertices: 0");
 	mesh->render(GL_TRIANGLES);
 	//disable render
 	phong->disable();
@@ -84,16 +113,6 @@ void Application::render(void)
 //called after render
 void Application::update(double seconds_elapsed)
 {
-
-	//Aquí vendrà el switch
-
-
-	/*
-	if (keystate[SDL_SCANCODE_SPACE])
-	{
-		model_matrix.rotateLocal(seconds_elapsed, Vector3(0,1,0));
-	}
-	*/
 	if (keystate[SDL_SCANCODE_W])
 	{
 		model_matrix.rotateLocal(seconds_elapsed, Vector3(1, 0, 0));
@@ -165,9 +184,18 @@ void Application::update(double seconds_elapsed)
 //keyboard press event 
 void Application::onKeyPressed( SDL_KeyboardEvent event )
 {
+	int t_count = 0;
 	switch(event.keysym.sym)
 	{
 		case SDLK_ESCAPE: exit(0); break; //ESC key, kill the app
+		case SDLK_j:
+			if (event.type == SDL_KEYUP){
+				printf ("Enter desired number of triangles: ");
+				scanf ("%d", &t_count);
+				printf ("Triangles: %d", t_count);
+				//TODO: execute surface simplification method
+			}
+			break;
 		case SDLK_z:
 			if (event.type == SDL_KEYUP) {
 				GLint previous[2];
